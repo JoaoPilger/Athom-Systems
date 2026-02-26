1e2dd08c109e87484ed8e6576eecba44f01c8d2d
YuriGermano <germanoyuri7@gmail.com>
2026-02-26 20:28:15 -0300

    Fiz o componente de produtos

--- AthomSystem/src/App.jsx
+++ AthomSystem/src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import './App.css'
+import ProductsCarousel from './components/produtos/produtos';
 
 function App() {
-  return(
-    <div></div>
-  )
+  return <ProductsCarousel />;
 }
 
-export default App
+export default App;
